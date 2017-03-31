3320ac91ada4ab79500bfd56db77845c99553aa4
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2017-03-31 17:49:21 +0200

    Queue with double stacks + memoisation of the smallest value

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -16,42 +16,137 @@ public class BestEducation
       this.output = output;
     }
 
+		public class OurStack
+    {
+      private int? currentMin;
+      private Stack<KeyValuePair<int, int>> stack;
+
+      public OurStack()
+      {
+        stack = new Stack<KeyValuePair<int, int>>();
+      }
+
+      public void EmptyMinimum()
+      {
+        currentMin = null;
+      }
+
+      public void Push(int val)
+      {
+        stack.Push(new KeyValuePair<int, int>(val, minValue(val)));
+      }
+
+      public int Pop()
+			{
+        return stack.Pop().Key;
+      }
+
+      public int? Min()
+      {
+        if (IsEmpty())
+        {
+          return null;
+        }
+        else
+        {
+          return stack.Peek().Value;
+        }
+      }
+
+      public bool IsEmpty()
+      {
+        return stack.Count == 0;
+      }
+
+      private int minValue(int val)
+      {
+        if (!this.currentMin.HasValue || val < this.currentMin)
+          this.currentMin = val;
+
+        return (int)this.currentMin;
+      }
+    }
+
+    public class OurQueue
+    {
+      private OurStack inbox, outbox;
+
+      public OurQueue()
+      {
+        inbox = new OurStack();
+        outbox = new OurStack();
+      }
+
+      public void Enqueue(int val)
+      {
+        inbox.Push(val);
+      }
+
+      public int Dequeue()
+      {
+        if (outbox.IsEmpty())
+        {
+          outbox.EmptyMinimum();
+          while (!inbox.IsEmpty())
+          {
+            outbox.Push(inbox.Pop());
+            inbox.EmptyMinimum();
+          }
+        }
+
+        return outbox.Pop();
+      }
+
+      public int Min()
+      {
+        if (inbox.IsEmpty())
+        {
+					return outbox.Min().Value;
+        }
+
+				if (outbox.IsEmpty())
+				{
+					return inbox.Min().Value;
+				}
+
+
+        if (inbox.Min() < outbox.Min())
+        {
+          return inbox.Min().Value;
+        }
+        else
+        {
+          return outbox.Min().Value;
+        }
+      }
+    }
+
     public void Solve()
     {
       int numCommands = ReadIntRow()[0];
 
-      var queue = new Queue<int>();
-
-      int? currentMinimum = null;
+      var queue = new OurQueue();
 
       for (int i = 0; i < numCommands; i++)
       {
         var command = ReadStrRow();
         if (command[0] == "+")
         {
-          int addVal = int.Parse(command[1]);
-          queue.Enqueue(addVal);
-
-          if (currentMinimum.HasValue && addVal < currentMinimum)
-            currentMinimum = addVal;
+          int addedValue = int.Parse(command[1]);
+          queue.Enqueue(addedValue);
         }
         else if (command[0] == "-")
         {
-          int removedElement = queue.Dequeue();
-
-          if (removedElement == currentMinimum)
-					  currentMinimum = null;
+          queue.Dequeue();
         }
         else
         {
-          if (!currentMinimum.HasValue)
-            currentMinimum = queue.Min();
-          
-          output.WriteLine(currentMinimum);
+          var minVal = queue.Min();
+          output.WriteLine(minVal);
         }
       }
     }
-
+   
     private List<int> ReadIntRow()
     {
       return input.ReadLine().Split(' ').Select(str => int.Parse(str)).ToList();
